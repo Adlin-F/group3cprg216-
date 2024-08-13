0cee2bbe3c578fa2123768e4e94c739c06b412d0
Adlin-F <adlin.firdaus@edu.sait.ca>
2024-08-13 16:29:14 -0600

    Add files via upload

---
+++ Project Classes/Getters for attributes.py
@@ -0,0 +1,15 @@
+ # Getters for attributes
+def get_isbn(self):
+        return self.__isbn
+
+def get_title(self):
+        return self.__title
+
+def get_author(self):
+        return self.__author
+
+def get_genre(self):
+        return self.__genre
+
+def get_available(self):
+        return self.__available
